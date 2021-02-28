ae74f94093dce341d2a98111251a25f4ec517d57
Фаниль Ганеев <wiper996100@gmail.com>
2021-02-28 22:24:17 +0600

    first commit

---
+++ pomodoro.js
@@ -0,0 +1,80 @@
+/**
+ * статус таймера
+ */
+const TIMER_STATS = {
+  pomodoro: "pomodoro",
+  break: "break",
+};
+/**изменение цвета фона */
+const BACKGROUNDS = {
+  [TIMER_STATS.pomodoro]: "linear-gradient(90deg, #F78CA0 0%, #F9748F 20.31%, #FD868C 66.67%, #FE9A8B 100%)",
+  [TIMER_STATS.break]: "linear-gradient(180deg, #48C6EF 0%, #6F86D6 100%)",
+};
+let radius = 58;
+
+//Цвет фона по умолчанию
+document.querySelector('body').style.backgroundImage = BACKGROUNDS.pomodoro
+
+
+/**
+ * State
+ */
+const state = {
+  minute: 0,
+  pomodoro: 2, //длина таймера
+  round: 0, // количество помидоров
+  breakShort: 1, // короткий перерыв
+  breakLong: 3, // длинный перерыв
+  cicle: 4, // количество циклов, через которые будет длинный перерыв
+  dashOffset: 2 * 3.14 * radius, //вычисляем длину окружности, по формуле P=2πR,где R-радиус
+  status: TIMER_STATS.pomodoro,
+  timerId: null, //id таймера setInterval
+  timerValue: 0, //текущее значение таймера (сек)
+  pause: false,
+  isOpen: false,
+};
+//подтверждаем изменение данных формы
+const handleSubmit = (event) => {
+  // code
+};
+//Запускает таймер
+const play = () => {
+  document.getElementById("play").style.display = 'none'
+  document.getElementById("pause").style.display = 'block'
+  pauseInterval = setInterval(() => {
+    ++state.timerValue
+    state.timerValue < 10 ? document.querySelector('.sec').innerHTML = '0' + state.timerValue : document.querySelector('.sec').innerHTML = state.timerValue
+    if (state.timerValue > 59) {
+      ++state.minute
+      state.timerValue = 00
+      document.querySelector('.min').innerHTML = '0' + state.minute
+    }
+  }, 300)
+};
+//ставит таймер на паузу
+const pause = () => {
+  document.getElementById("play").style.display = 'none'
+  document.getElementById("pause").style.display = 'none'
+  document.getElementById("continue").style.display = 'block'
+  document.getElementById("stop").style.display = 'block'
+  clearInterval(pauseInterval);
+};
+//останавливает таймер
+const stop = () => {
+  document.getElementById("play").style.display = 'block'
+  document.getElementById("pause").style.display = 'none'
+  document.getElementById("continue").style.display = 'none'
+  document.getElementById("stop").style.display = 'none'
+};
+//Продолжить 
+const cont = () => {
+  document.getElementById("play").style.display = 'none'
+  document.getElementById("pause").style.display = 'block'
+  document.getElementById("continue").style.display = 'none'
+  document.getElementById("stop").style.display = 'none'
+  play()
+};
+//меняет значение и статус таймера
+tick = () => {
+  // code
+};
