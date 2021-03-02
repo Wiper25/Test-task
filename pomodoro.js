d3722577e97ca67ec011f1b1f91adc78dc8fff0a
Фаниль Ганеев <wiper996100@gmail.com>
2021-03-02 23:42:53 +0600

    UpDate

--- pomodoro.js
+++ pomodoro.js
@@ -10,7 +10,7 @@ const BACKGROUNDS = {
   [TIMER_STATS.pomodoro]: "linear-gradient(90deg, #F78CA0 0%, #F9748F 20.31%, #FD868C 66.67%, #FE9A8B 100%)",
   [TIMER_STATS.break]: "linear-gradient(180deg, #48C6EF 0%, #6F86D6 100%)",
 };
-let radius = 58;
+let radius = 150;
 
 //Цвет фона по умолчанию
 document.querySelector('body').style.backgroundImage = BACKGROUNDS.pomodoro
@@ -20,7 +20,6 @@ document.querySelector('body').style.backgroundImage = BACKGROUNDS.pomodoro
  * State
  */
 const state = {
-  minute: 0,
   pomodoro: 2, //длина таймера
   round: 0, // количество помидоров
   breakShort: 1, // короткий перерыв
@@ -30,27 +29,29 @@ const state = {
   status: TIMER_STATS.pomodoro,
   timerId: null, //id таймера setInterval
   timerValue: 0, //текущее значение таймера (сек)
-  pause: false,
-  isOpen: false,
 };
+
 //подтверждаем изменение данных формы
 const handleSubmit = (event) => {
-  // code
+
 };
 //Запускает таймер
 const play = () => {
+  let min = 0
   document.getElementById("play").style.display = 'none'
   document.getElementById("pause").style.display = 'block'
   pauseInterval = setInterval(() => {
     ++state.timerValue
     state.timerValue < 10 ? document.querySelector('.sec').innerHTML = '0' + state.timerValue : document.querySelector('.sec').innerHTML = state.timerValue
-    if (state.timerValue > 59) {
-      ++state.minute
-      state.timerValue = 00
-      document.querySelector('.min').innerHTML = '0' + state.minute
+    if (state.timerValue == 59) {
+      state.timerValue = 0
+      ++min
+      min < 10 ? document.querySelector('.min').innerHTML = '0' + min : document.querySelector('.min').innerHTML = min
     }
-  }, 300)
+    console.log(state.timerValue)
+  }, 10)
 };
+
 //ставит таймер на паузу
 const pause = () => {
   document.getElementById("play").style.display = 'none'
